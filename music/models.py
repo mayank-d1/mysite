ad812939905353855a709e9f527a5266095f7786
Mayank Diwedi <diwedi.mayank1@gmail.com>
2018-02-23 20:46:06 +0530

    Latest Changes

--- music/models.py
+++ music/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
@@ -7,7 +8,10 @@ class Album(models.Model):
     artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
-    album_logo = models.CharField(max_length=100)
+    album_logo = models.FileField()
+
+    def get_absolute_url(self):
+        return reverse('music:detail', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.album_title + " - " + self.artist
@@ -17,3 +21,7 @@ class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
+    is_favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.song_title
--- music/urls.py
+++ music/urls.py
@@ -1,6 +1,19 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    # music/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    # registration
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+    # music/<album_id>/
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    # music/album/add
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+    # music/album/2
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+    # music/album/2/delete
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
 ]
